e7eb888e4d0bbffe1b1e56c32a28a699085dc006
user <user@DESKTOP-6BUB7S3>
2024-04-24 12:24:39 +0900

    첫번째 커밋

---
+++ cosmetic/src/test/java/com/example/cosmetic/CosmeticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cosmetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
