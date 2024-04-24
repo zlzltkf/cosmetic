82f261b5b06d7486a0cfe700c26f30c684025a51
user <user@DESKTOP-T33P0L7>
2024-04-24 14:46:29 +0900

    템플릿 추가 및 메인화면

--- cosmetic/src/main/java/com/example/cosmetic/CosmeticApplication.java
+++ cosmetic/src/main/java/com/example/cosmetic/CosmeticApplication.java
@@ -1,7 +1,17 @@
 package com.example.cosmetic;
 
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
 
 @SpringBootApplication
 public class CosmeticApplication {
@@ -10,4 +20,17 @@ public class CosmeticApplication {
 		SpringApplication.run(CosmeticApplication.class, args);
 	}
 
+	@Bean
+	SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+	      SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+	      bean.setDataSource(dataSource);
+	      Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/**/*.xml");
+	      bean.setMapperLocations(res);
+	      return bean.getObject();
+	   }
+
+	   @Bean
+	   SqlSessionTemplate sqlSession(SqlSessionFactory factory) {
+	      return new SqlSessionTemplate(factory);
+	   } 
 }
