376648207661c163677a10f257432f33169753b9
user <user@DESKTOP-6BUB7S3>
2024-04-25 16:44:28 +0900

    implements 삭제했어요

--- cosmetic/src/main/java/com/example/cosmetic/model/member/MemberDAO.java
+++ cosmetic/src/main/java/com/example/cosmetic/model/member/MemberDAO.java
@@ -1,8 +1,23 @@
 package com.example.cosmetic.model.member;
 
-public interface MemberDAO {
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO{
 	
-	String login (String userid,String passwd);
-	
-	
+	@Autowired
+	SqlSession session;
+
+	public String login(String userid, String passwd) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("userid", userid);
+		map.put("passwd", passwd);
+		return session.selectOne("member.login", map);
+	}
+
 }
