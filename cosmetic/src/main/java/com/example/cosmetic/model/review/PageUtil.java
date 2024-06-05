3e416537b7b4c050dc5debcae7cb7fdd3d0b05dc
user <user@DESKTOP-KPVCFA5>
2024-06-05 16:25:56 +0900

    승아 - 리뷰 사진

--- cosmetic/src/main/java/com/example/cosmetic/model/review/PageUtil.java
+++ cosmetic/src/main/java/com/example/cosmetic/model/review/PageUtil.java
@@ -1,7 +1,7 @@
 package com.example.cosmetic.model.review;
 
 public class PageUtil {
-   public static final int PAGE_SCALE = 3; //수정
+   public static final int PAGE_SCALE = 7; //수정
    //페이지당 게시물 수
    
    public static final int BLOCK_SCALE = 3; //수정
